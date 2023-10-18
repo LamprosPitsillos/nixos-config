8747b371fbb9cc8d249e05204c30c14307866dd8
Lampros Pitsillos <hauahx@gmail.com>
2023-10-18 14:41:12 +0300

    Fixed font not having different variants

--- home-manager/programs/kitty/kitty.nix
+++ home-manager/programs/kitty/kitty.nix
@@ -1,9 +1,9 @@
 {pkgs, ...}: {
-  programs.kitty = {
+  programs.kitty = let font_family = "JetBrainsMono NF"; in {
     enable = true;
     font = {
       package = pkgs.nerdfonts;
-      name = "JetBrainsMono NF";
+      name = font_family;
       size = 12;
     };
     keybindings = {
@@ -27,6 +27,9 @@
       # mouse_map ctrl+shift+right press ungrabbed combine : mouse_select_command_output : kitty_scrollback_nvim --config-file get_text_last_visited_cmd_output.lua
     '';
     settings = {
+    bold_font = "${font_family} Bold";
+    italic_font = "${font_family} Italic";
+    bold_italic_font = "${font_family} Bold Italic";
       disable_ligatures = "cursor";
       # scrollback_pager = ''
       # bash -c "exec nvim 63<&0 0</dev/null -u NONE -c 'map <silent> q :qa!<CR>' -c 'map <silent> i :qa!<CR>' -c 'set shell=bash scrollback=100000 termguicolors laststatus=0 cmdheight=0 noruler noshowmode noshowcmd clipboard+=unnamed' -c 'autocmd TermEnter * stopinsert' -c 'autocmd TermClose * call cursor(max([0,INPUT_LINE_NUMBER-1])+CURSOR_LINE, CURSOR_COLUMN)' -c 'terminal sed </dev/fd/63 -e \"s/'$'\x1b'']8;;file:[^\]*[\]//g\" && sleep 0.01 && printf \"'$'\x1b'']2;\"'"
