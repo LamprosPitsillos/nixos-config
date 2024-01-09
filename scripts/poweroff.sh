f3fa8da8c8409efc4ddaf3815103ec97fcf823f8
Lampros Pitsillos <hauahx@gmail.com>
2024-01-10 01:31:05 +0200

    no more plain scripts,todo write in real language

--- scripts/poweroff.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-RET=$(printf "󰐥 Shutdown\n󰜉 Reboot\n󰌾 Lock\n󰅚 Cancel" | tofi --padding-left=10% --padding-right=10%)
-case $RET in
-	*Shutdown) notify-send "System" "The PC is <span color=\"red\"><b>shuting</b></span>\ down!" -i "system-shutdown" || systemctl poweroff ;;
-	*Reboot) notify-send "System" "The PC is <span color=\"orange\"><b>rebooting</b></span>\!" -i "system-restart"   || systemctl reboot;;
-	*Lock) notify-send "System" "The PC is <span color=\"orange\"><b>rebooting</b></span>\!" -i "system-lock-screen"     || gtklock ;;
-	*) ;;
-esac
