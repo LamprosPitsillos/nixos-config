133d596ae25c148cb728bf656a09d0c0a2c48a66
Lampros Pitsillos <hauahx@gmail.com>
2024-01-10 01:23:52 +0200

    fmt

--- home-manager/programs/wm/hyprland.nix
+++ home-manager/programs/wm/hyprland.nix
@@ -12,242 +12,242 @@
       in
         /* hyprlang */
       ''
-      #--------------------------------------------------------------------#
-      #                                Docs                                #
-      #               https://wiki.hyprland.org/Configuring                #
-      #--------------------------------------------------------------------#
+        #--------------------------------------------------------------------#
+        #                                Docs                                #
+        #               https://wiki.hyprland.org/Configuring                #
+        #--------------------------------------------------------------------#
 
-      monitor=eDP-1,1920x1080,0x0 ,1
-      monitor=,preferred,auto,1
+        monitor=eDP-1,1920x1080,0x0 ,1
+        monitor=,preferred,auto,1
 
-      env = XCURSOR_SIZE,20
+        env = XCURSOR_SIZE,20
 
-      source = ~/.config/hypr/monitors.conf
+        source = ~/.config/hypr/monitors.conf
 
-      exec-once =  hyprpaper &
-      exec-once =  swaync &
-      exec-once = eww open bar & # hyprpaper & firefox
-      exec-once = hyprctl setcursor "Bibata-Modern-Ice" 8
+        exec-once =  hyprpaper &
+        exec-once =  swaync &
+        exec-once = eww open bar & # hyprpaper & firefox
+        exec-once = hyprctl setcursor "Bibata-Modern-Ice" 8
 
-      misc {
-      close_special_on_empty=true
-      disable_splash_rendering = true
-      disable_hyprland_logo = true
-      enable_swallow=true
-      animate_manual_resizes=false
-      swallow_regex=^(kitty)$
-      }
-      # For all categories, see https://wiki.hyprland.org/Configuring/Variables/
-      input {
-          kb_layout = us,gr
-          kb_variant =
-          kb_model =
-          kb_options = caps:escape
-          kb_rules =
-          numlock_by_default=true
-          repeat_rate=20
-          repeat_delay=400
-          follow_mouse = 1
+        misc {
+        close_special_on_empty=true
+        disable_splash_rendering = true
+        disable_hyprland_logo = true
+        enable_swallow=true
+        animate_manual_resizes=false
+        swallow_regex=^(kitty)$
+        }
+        # For all categories, see https://wiki.hyprland.org/Configuring/Variables/
+        input {
+            kb_layout = us,gr
+            kb_variant =
+            kb_model =
+            kb_options = caps:escape
+            kb_rules =
+            numlock_by_default=true
+            repeat_rate=20
+            repeat_delay=400
+            follow_mouse = 1
 
-          touchpad {
-              natural_scroll = true
-              disable_while_typing = true
-          }
+            touchpad {
+                natural_scroll = true
+                disable_while_typing = true
+            }
 
-          sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
-      }
+            sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
+        }
 
-      general {
-          # See https://wiki.hyprland.org/Configuring/Variables/ for more
+        general {
+            # See https://wiki.hyprland.org/Configuring/Variables/ for more
 
-          gaps_in = 3
-          gaps_out = 10
-          gaps_workspaces = 40
+            gaps_in = 3
+            gaps_out = 10
+            gaps_workspaces = 40
 
-          border_size = 2
-          col.active_border = rgba(FFB53AEE) rgba(EF990EEE) 45deg
-          col.inactive_border = rgba(595959aa)
-          cursor_inactive_timeout=5
-          layout = dwindle
-      }
+            border_size = 2
+            col.active_border = rgba(FFB53AEE) rgba(EF990EEE) 45deg
+            col.inactive_border = rgba(595959aa)
+            cursor_inactive_timeout=5
+            layout = dwindle
+        }
 
-      decoration {
-          # See https://wiki.hyprland.org/Configuring/Variables/ for more
+        decoration {
+            # See https://wiki.hyprland.org/Configuring/Variables/ for more
 
-          rounding = 4
+            rounding = 4
 
-          drop_shadow = true
-          shadow_range = 6
-          shadow_render_power = 3
-          col.shadow = rgba(1a1a1aee)
+            drop_shadow = true
+            shadow_range = 6
+            shadow_render_power = 3
+            col.shadow = rgba(1a1a1aee)
 
-          blur {
+            blur {
 
-              enabled = true
-              size = 3
-              passes = 1
-              new_optimizations = true
-          }
-      }
+                enabled = true
+                size = 3
+                passes = 1
+                new_optimizations = true
+            }
+        }
 
-      animations {
-          enabled = true
-          # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+        animations {
+            enabled = true
+            # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
 
-          bezier = myBezier, 0.05, 0.9, 0.1, 1.05
+            bezier = myBezier, 0.05, 0.9, 0.1, 1.05
 
-          animation = windows, 1, 2, myBezier
-          animation = windowsOut, 1, 2, default, popin 80%
-          animation = border, 1, 10, default
-          animation = borderangle, 1, 8, default
-          animation = fade, 1 , 1, default
-          animation = workspaces, 1, 4, default
-          animation = specialWorkspace, 1, 4, default, fade
-      }
+            animation = windows, 1, 2, myBezier
+            animation = windowsOut, 1, 2, default, popin 80%
+            animation = border, 1, 10, default
+            animation = borderangle, 1, 8, default
+            animation = fade, 1 , 1, default
+            animation = workspaces, 1, 4, default
+            animation = specialWorkspace, 1, 4, default, fade
+        }
 
-      dwindle {
-          # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
-          pseudotile = true # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-          preserve_split = true # you probably want this
-          special_scale_factor = 0.8
-      }
+        dwindle {
+            # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
+            pseudotile = true # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+            preserve_split = true # you probably want this
+            special_scale_factor = 0.8
+        }
 
-      master {
-          # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
-          new_is_master = true
-          special_scale_factor = 0.8
-
-
-          # bind=$mainMod SHIFT,j,layoutmsg,swapnext
-          # bind=$mainMod SHIFT,k,layoutmsg,swapprev
-          # bind=$mainMod SHIFT,h,layoutmsg,addmaster
-          # bind=$mainMod SHIFT,l,layoutmsg,removemaster
-          # bind=$mainMod SHIFT,m,layoutmsg,swapwithmaster
-
-      }
-
-      gestures {
-          # See https://wiki.hyprland.org/Configuring/Variables/ for more
-          workspace_swipe = false
-      }
-
-      # See https://wiki.hyprland.org/Configuring/Keywords/#executing for more
-      device:epic mouse V1 {
-          sensitivity = -0.5
-      }
-
-      # Example windowrule v1
-      # windowrule = float, ^(kitty)$
-
-      windowrulev2 = noinitialfocus,class:^(com-eteks-sweethome3d-SweetHome3DBootstrap)$
-      windowrulev2 = nofocus,class:^(com-eteks-sweethome3d-SweetHome3DBootstrap)$,title:^(win1)$
+        master {
+            # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
+            new_is_master = true
+            special_scale_factor = 0.8
 
 
-      # Example windowrule v2
-      windowrulev2 = fakefullscreen,workspace special:music_player,class:^(mpv)$
-      windowrulev2 = float,class:nm-connection-editor
-      # windowrulev2 = workspace +1 ,onworkspace:1,workspace special:music_player
-      windowrulev2 = pin,class:swappy
+            # bind=$mainMod SHIFT,j,layoutmsg,swapnext
+            # bind=$mainMod SHIFT,k,layoutmsg,swapprev
+            # bind=$mainMod SHIFT,h,layoutmsg,addmaster
+            # bind=$mainMod SHIFT,l,layoutmsg,removemaster
+            # bind=$mainMod SHIFT,m,layoutmsg,swapwithmaster
 
-      # windowrulev2 = dimaround,fullscreen:1
-      # windowrulev2 = bordersize 8,fullscreen:1
-      # windowrulev2 = move %100 %100,class:nm-connection-editor
-      # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
+        }
 
+        gestures {
+            # See https://wiki.hyprland.org/Configuring/Variables/ for more
+            workspace_swipe = false
+        }
 
-      # See https://wiki.hyprland.org/Configuring/Keywords/ for more
-      $mainMod = SUPER
+        # See https://wiki.hyprland.org/Configuring/Keywords/#executing for more
+        device:epic mouse V1 {
+            sensitivity = -0.5
+        }
 
-      # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
-      bind = $mainMod,return, exec, kitty
-      #bind = $mainMod, period , exec,[workspace special:terminal] kitty
-      bind = $mainMod, period , exec, ${scripts.scratchpad} terminal "kitty "
-      bind =,Menu ,exec, hyprctl switchxkblayout kanata next
-      bind =,Print ,exec, ${system_scripts.screen_shot} full
-      bind =SHIFT,Print ,exec, ${system_scripts.screen_shot} area
-      bind =$mainMod,Print ,exec,${system_scripts.screen_rec} full
-      bind =$mainMod SHIFT,Print ,exec,${system_scripts.screen_rec} area
-      bind =$mainMod,B ,exec,eww open --toggle bar
-      bind =$mainMod SHIFT,S , exec, ${scripts.toggle} "misc:enable_swallow" "int" "1" "0"
+        # Example windowrule v1
+        # windowrule = float, ^(kitty)$
 
-      #grim -g "$(slurp)" - | swappy -f - -o $HOME/pics/Screenshot/"$(date +'%Y-%m-%d_%H-%M-%S')_$(echo | tofi --prompt-text="Name: " --require-match=false --height=8% | tr " " "_")"
-      bind = $mainMod, W, killactive,
-      bind = $mainMod SHIFT, P ,pin
-
-      bind = $mainMod, Z,exec, ${scripts.toggle} "misc:cursor_zoom_factor" "float" "1.00000" "4.00000"
-      bind = $mainMod ALT, X, exec , eww open --toggle bar && eww open --toggle powermenu
-      bind = $mainMod SHIFT, q, exit,
-      bind = $mainMod, E, exec, thunar
-      bind = $mainMod, comma , exec, ${scripts.scratchpad} file_manager "kitty --class file_manager ${pkgs.yazi}/bin/yazi"
-      bind = $mainMod, M , exec, ${scripts.scratchpad} music_player "kitty ${system_scripts.mpv_music_player}"
-      bind = $mainMod, T, togglefloating,
-      bind = $mainMod, F , fullscreen,0
-      bind = $mainMod SHIFT, F, fakefullscreen,
-      bind = $mainMod, space, exec, $( tofi-drun )
-      bind = $mainMod, P, pseudo
-      bind = $mainMod, s,layoutmsg, togglesplit
-      # bind = $mainMod, I , exec, [workspace 1;] kitty
-
-      #
-      # bind = $mainMod SHIFT, I , movetoworkspace,special:fullscreen
-      # bind = $mainMod CTRL, I , movetoworkspace,
-      # bind = $mainMod, I , togglespecialworkspace,fullscreen
-
-      # Move focus with mainMod + arrow keys
-      bind = $mainMod, h, movefocus, l
-      bind = $mainMod, l, movefocus, r
-      bind = $mainMod, k, movefocus, u
-      bind = $mainMod, j, movefocus, d
-
-      # Switch workspaces with mainMod + [0-9]
-      bind = $mainMod, 1, workspace, 1
-      bind = $mainMod, 2, workspace, 2
-      bind = $mainMod, 3, workspace, 3
-      bind = $mainMod, 4, workspace, 4
-      bind = $mainMod, 5, workspace, 5
-
-      # Move active window to a workspace with mainMod + SHIFT + [0-9]
-      bind = $mainMod SHIFT, 1, movetoworkspace, 1
-      bind = $mainMod SHIFT, 2, movetoworkspace, 2
-      bind = $mainMod SHIFT, 3, movetoworkspace, 3
-      bind = $mainMod SHIFT, 4, movetoworkspace, 4
-      bind = $mainMod SHIFT, 5, movetoworkspace, 5
-
-      bind = $mainMod SHIFT, h, movewindow, l
-      bind = $mainMod SHIFT, j, movewindow, d
-      bind = $mainMod SHIFT, k, movewindow, u
-      bind = $mainMod SHIFT, l, movewindow, r
+        windowrulev2 = noinitialfocus,class:^(com-eteks-sweethome3d-SweetHome3DBootstrap)$
+        windowrulev2 = nofocus,class:^(com-eteks-sweethome3d-SweetHome3DBootstrap)$,title:^(win1)$
 
 
-      binde= ,XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1.0
-      binde= ,XF86AudioLowerVolume, exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- -l 1.0
-      binde= ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+        # Example windowrule v2
+        windowrulev2 = fakefullscreen,workspace special:music_player,class:^(mpv)$
+        windowrulev2 = float,class:nm-connection-editor
+        # windowrulev2 = workspace +1 ,onworkspace:1,workspace special:music_player
+        windowrulev2 = pin,class:swappy
 
-      binde= ,XF86MonBrightnessDown,exec,brightnessctl set 100- -q
-      binde= ,XF86MonBrightnessUp,exec,brightnessctl set 100+ -q
-
-      bindl=,XF86AudioPlay,exec,${system_scripts.mpv_controller} toggle
-      bindl=,XF86AudioPrev,exec,${system_scripts.mpv_controller} prev
-      bindl=,XF86AudioNext,exec,${system_scripts.mpv_controller} next
-      # Scroll through existing workspaces with mainMod + scroll
-      bind = $mainMod, mouse_down, workspace, e+1
-      bind = $mainMod, mouse_up, workspace, e-1
-
-      bind=$mainMod,o,submap,open
-
-      submap=open
-      bind=,Q,exec,$scripts/qute_search.sh
-      bind=,Q,submap,reset
-      bind=,S,exec,${pkgs.zathura}/bin/zathura /home/inferno/UoC/9ο\ Εξαμηνο/possible_courses.pdf
-      bind=,S,submap,reset
-
-      bind=,escape,submap,reset
-      submap=reset
+        # windowrulev2 = dimaround,fullscreen:1
+        # windowrulev2 = bordersize 8,fullscreen:1
+        # windowrulev2 = move %100 %100,class:nm-connection-editor
+        # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
 
 
-      # Move/resize windows with mainMod + LMB/RMB and dragging
-      bindm = $mainMod, mouse:272, movewindow
-      bindm = $mainMod SHIFT, mouse:272, resizewindow
+        # See https://wiki.hyprland.org/Configuring/Keywords/ for more
+        $mainMod = SUPER
+
+        # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
+        bind = $mainMod,return, exec, kitty
+        #bind = $mainMod, period , exec,[workspace special:terminal] kitty
+        bind = $mainMod, period , exec, ${scripts.scratchpad} terminal "kitty "
+        bind =,Menu ,exec, hyprctl switchxkblayout kanata next
+        bind =,Print ,exec, ${system_scripts.screen_shot} full
+        bind =SHIFT,Print ,exec, ${system_scripts.screen_shot} area
+        bind =$mainMod,Print ,exec,${system_scripts.screen_rec} full
+        bind =$mainMod SHIFT,Print ,exec,${system_scripts.screen_rec} area
+        bind =$mainMod,B ,exec,eww open --toggle bar
+        bind =$mainMod SHIFT,S , exec, ${scripts.toggle} "misc:enable_swallow" "int" "1" "0"
+
+        #grim -g "$(slurp)" - | swappy -f - -o $HOME/pics/Screenshot/"$(date +'%Y-%m-%d_%H-%M-%S')_$(echo | tofi --prompt-text="Name: " --require-match=false --height=8% | tr " " "_")"
+        bind = $mainMod, W, killactive,
+        bind = $mainMod SHIFT, P ,pin
+
+        bind = $mainMod, Z,exec, ${scripts.toggle} "misc:cursor_zoom_factor" "float" "1.00000" "4.00000"
+        bind = $mainMod ALT, X, exec , eww open --toggle bar && eww open --toggle powermenu
+        bind = $mainMod SHIFT, q, exit,
+        bind = $mainMod, E, exec, thunar
+        bind = $mainMod, comma , exec, ${scripts.scratchpad} file_manager "kitty --class file_manager ${pkgs.yazi}/bin/yazi"
+        bind = $mainMod, M , exec, ${scripts.scratchpad} music_player "kitty ${system_scripts.mpv_music_player}"
+        bind = $mainMod, T, togglefloating,
+        bind = $mainMod, F , fullscreen,0
+        bind = $mainMod SHIFT, F, fakefullscreen,
+        bind = $mainMod, space, exec, $( tofi-drun )
+        bind = $mainMod, P, pseudo
+        bind = $mainMod, s,layoutmsg, togglesplit
+        # bind = $mainMod, I , exec, [workspace 1;] kitty
+
+        #
+        # bind = $mainMod SHIFT, I , movetoworkspace,special:fullscreen
+        # bind = $mainMod CTRL, I , movetoworkspace,
+        # bind = $mainMod, I , togglespecialworkspace,fullscreen
+
+        # Move focus with mainMod + arrow keys
+        bind = $mainMod, h, movefocus, l
+        bind = $mainMod, l, movefocus, r
+        bind = $mainMod, k, movefocus, u
+        bind = $mainMod, j, movefocus, d
+
+        # Switch workspaces with mainMod + [0-9]
+        bind = $mainMod, 1, workspace, 1
+        bind = $mainMod, 2, workspace, 2
+        bind = $mainMod, 3, workspace, 3
+        bind = $mainMod, 4, workspace, 4
+        bind = $mainMod, 5, workspace, 5
+
+        # Move active window to a workspace with mainMod + SHIFT + [0-9]
+        bind = $mainMod SHIFT, 1, movetoworkspace, 1
+        bind = $mainMod SHIFT, 2, movetoworkspace, 2
+        bind = $mainMod SHIFT, 3, movetoworkspace, 3
+        bind = $mainMod SHIFT, 4, movetoworkspace, 4
+        bind = $mainMod SHIFT, 5, movetoworkspace, 5
+
+        bind = $mainMod SHIFT, h, movewindow, l
+        bind = $mainMod SHIFT, j, movewindow, d
+        bind = $mainMod SHIFT, k, movewindow, u
+        bind = $mainMod SHIFT, l, movewindow, r
+
+
+        binde= ,XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1.0
+        binde= ,XF86AudioLowerVolume, exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- -l 1.0
+        binde= ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+
+        binde= ,XF86MonBrightnessDown,exec,brightnessctl set 100- -q
+        binde= ,XF86MonBrightnessUp,exec,brightnessctl set 100+ -q
+
+        bindl=,XF86AudioPlay,exec,${system_scripts.mpv_controller} toggle
+        bindl=,XF86AudioPrev,exec,${system_scripts.mpv_controller} prev
+        bindl=,XF86AudioNext,exec,${system_scripts.mpv_controller} next
+        # Scroll through existing workspaces with mainMod + scroll
+        bind = $mainMod, mouse_down, workspace, e+1
+        bind = $mainMod, mouse_up, workspace, e-1
+
+        bind=$mainMod,o,submap,open
+
+        submap=open
+        bind=,Q,exec,${system_scripts.fuzzy_browser}
+        bind=,Q,submap,reset
+        bind=,S,exec,${pkgs.zathura}/bin/zathura /home/inferno/UoC/9ο\ Εξαμηνο/possible_courses.pdf
+        bind=,S,submap,reset
+
+        bind=,escape,submap,reset
+        submap=reset
+
+
+        # Move/resize windows with mainMod + LMB/RMB and dragging
+        bindm = $mainMod, mouse:272, movewindow
+        bindm = $mainMod SHIFT, mouse:272, resizewindow
 
       '';
   };
--- scripts/default.nix
+++ scripts/default.nix
@@ -1,91 +1,102 @@
 { pkgs
 , lib
 }: {
-    screen_rec = lib.getExe ( pkgs.writeShellApplication {
-        name = "sh_screen_rec";
-    runtimeInputs = with pkgs; [ slurp wl-screenrec yazi mpv ripdrag tofi libnotify];
+  screen_rec = lib.getExe (pkgs.writeShellApplication {
+    name = "sh_screen_rec";
+    runtimeInputs = with pkgs; [ slurp wl-screenrec yazi mpv ripdrag tofi libnotify ];
     text = ''
-        SCREEN_REC_DIR="$HOME/vids/Screen_rec"
+      SCREEN_REC_DIR="$HOME/vids/Screen_rec"
 
-        start_screen_recording() {
-            name="$1"
-            screen_rec_name="$(date +'%Y-%m-%d_%H-%M-%S')_$name"
-            path="$SCREEN_REC_DIR/$screen_rec_name.mp4"
-            case "$2" in
-                area) wl-screenrec -g "$(slurp)" -f "$path" ;;
-                full) wl-screenrec -f "$path" ;;
-            esac
-        }
+      start_screen_recording() {
+          name="$1"
+          screen_rec_name="$(date +'%Y-%m-%d_%H-%M-%S')_$name"
+          path="$SCREEN_REC_DIR/$screen_rec_name.mp4"
+          case "$2" in
+              area) wl-screenrec -g "$(slurp)" -f "$path" ;;
+              full) wl-screenrec -f "$path" ;;
+          esac
+      }
 
-        if pkill -SIGINT wl-screenrec; then
-            # shellcheck disable=SC2012
-            screen_rec_name=$(ls -t "$SCREEN_REC_DIR" | head --lines=1)
-            screen_rec="$SCREEN_REC_DIR/$screen_rec_name"
-            cmd=$(notify-send "Screen Recording" "Screen Rec Done" \
-                    -A "ripdrag  $screen_rec"=Drag \
-                    -A "mpv  $screen_rec"=Open \
-                    -A "yazi  $SCREEN_REC_DIR"=Goto)
-            eval "$cmd"
-        else
-            name="$(echo | tofi --prompt-text='Name: ' --require-match=false --height=8% | tr ' ' '_')"
-            if [ -z "$name" ];then
-                notify-send "Screen Recording" "Screen Rec canceled, no name given."
-                exit
-            fi
-            start_screen_recording "$name" "$1"
-        fi
-            '';
-    } );
-    screen_shot = lib.getExe ( pkgs.writeShellApplication {
-        name = "sh_screen_shot";
-    runtimeInputs = with pkgs;[ tofi grim swappy slurp libnotify];
+      if pkill -SIGINT wl-screenrec; then
+          # shellcheck disable=SC2012
+          screen_rec_name=$(ls -t "$SCREEN_REC_DIR" | head --lines=1)
+          screen_rec="$SCREEN_REC_DIR/$screen_rec_name"
+          cmd=$(notify-send "Screen Recording" "Screen Rec Done" \
+                  -A "ripdrag  $screen_rec"=Drag \
+                  -A "mpv  $screen_rec"=Open \
+                  -A "yazi  $SCREEN_REC_DIR"=Goto)
+          eval "$cmd"
+      else
+          name="$(echo | tofi --prompt-text='Name: ' --require-match=false --height=8% | tr ' ' '_')"
+          if [ -z "$name" ];then
+              notify-send "Screen Recording" "Screen Rec canceled, no name given."
+              exit
+          fi
+          start_screen_recording "$name" "$1"
+      fi
+    '';
+  });
+  screen_shot = lib.getExe (pkgs.writeShellApplication {
+    name = "sh_screen_shot";
+    runtimeInputs = with pkgs;[ tofi grim swappy slurp libnotify ];
     text =
-            ''
-            SCREEN_SHOT_DIR=$HOME/pics/Screenshot
-            name=$(echo | tofi  --prompt-text="Name: " --require-match=false --height=8% | tr " " "_")
-            if [ -z "$name" ];then
-                notify-send "Screen Shot" "Screen Shot canceled, no name given."
-                exit
-            fi
-            case "$1" in
-                    area) grim  -g "$(slurp)" - | swappy  -f - -o "$SCREEN_SHOT_DIR/$(date +'%Y-%m-%d_%H-%M-%S')_$name".png ;;
-                    full) sleep 0.2 && grim  - | swappy -f - -o "$SCREEN_SHOT_DIR/$(date +'%Y-%m-%d_%H-%M-%S')_$name".png ;;
-            esac
-            '';
+      ''
+        SCREEN_SHOT_DIR=$HOME/pics/Screenshot
+        name=$(echo | tofi  --prompt-text="Name: " --require-match=false --height=8% | tr " " "_")
+        if [ -z "$name" ];then
+            notify-send "Screen Shot" "Screen Shot canceled, no name given."
+            exit
+        fi
+        case "$1" in
+                area) grim  -g "$(slurp)" - | swappy  -f - -o "$SCREEN_SHOT_DIR/$(date +'%Y-%m-%d_%H-%M-%S')_$name".png ;;
+                full) sleep 0.2 && grim  - | swappy -f - -o "$SCREEN_SHOT_DIR/$(date +'%Y-%m-%d_%H-%M-%S')_$name".png ;;
+        esac
+      '';
 
-    } );
-    mpv_music_player = lib.getExe ( pkgs.writeShellApplication {
-        name = "sh_mpv_music_player"; /* sh */
-    runtimeInputs = with pkgs; [fd mpv libnotify fzf ];
+  });
+  mpv_music_player = lib.getExe (pkgs.writeShellApplication {
+    name = "sh_mpv_music_player"; /* sh */
+    runtimeInputs = with pkgs; [ fd mpv libnotify fzf ];
     text = ''
-    while true ;do
-        song="$(fd . --base-directory="/home/inferno/music" --color=always --strip-cwd-prefix | fzf  --ansi --border --height=100% --preview="ffprobe -hide_banner ~/music/{} || exa ~/music/{}" )" #
-        # shellcheck disable=SC2016
-        mpv --input-ipc-server=/tmp/mpvsocket \
-            --shuffle ~/music/"$song" \
-            --msg-module \
-            --term-title='󰎈 Playing: ''${filename} 󰎈'
-        notify-send "MPV music" "Playlist has finished."
-    done
+      while true ;do
+          song="$(fd . --base-directory="/home/inferno/music" --color=always --strip-cwd-prefix | fzf  --ansi --border --height=100% --preview="ffprobe -hide_banner ~/music/{} || exa ~/music/{}" )" #
+          # shellcheck disable=SC2016
+          mpv --input-ipc-server=/tmp/mpvsocket \
+              --shuffle ~/music/"$song" \
+              --msg-module \
+              --term-title='󰎈 Playing: ''${filename} 󰎈'
+          notify-send "MPV music" "Playlist has finished."
+      done
 
     '';
 
-    } );
-    mpv_controller = lib.getExe (pkgs.writeShellApplication {
-        name = "sh_mpv_controller";
-    runtimeInputs = with pkgs; [socat libnotify];
+  });
+  mpv_controller = lib.getExe (pkgs.writeShellApplication {
+    name = "sh_mpv_controller";
+    runtimeInputs = with pkgs; [ socat libnotify ];
     text = ''
-    case "$1" in
-        next) echo 'playlist-next' | socat - /tmp/mpvsocket && notify-send MPV "Next song 󰙡" -i "mpv"
-            ;;
-        prev) echo 'playlist-prev' | socat - /tmp/mpvsocket && notify-send MPV "Prev song 󰙣" -i "mpv"
-            ;;
-        toggle) echo 'cycle pause' | socat - /tmp/mpvsocket && notify-send MPV "Toggle pause 󰏤" -i "mpv"
-            ;;
-        *) notify-send Error "\"$1\" is not a valid command"
-            ;;
-    esac
+      case "$1" in
+          next) echo 'playlist-next' | socat - /tmp/mpvsocket && notify-send MPV "Next song 󰙡" -i "mpv"
+              ;;
+          prev) echo 'playlist-prev' | socat - /tmp/mpvsocket && notify-send MPV "Prev song 󰙣" -i "mpv"
+              ;;
+          toggle) echo 'cycle pause' | socat - /tmp/mpvsocket && notify-send MPV "Toggle pause 󰏤" -i "mpv"
+              ;;
+          *) notify-send Error "\"$1\" is not a valid command"
+              ;;
+      esac
     '';
-   }
-   );
+  }
+  );
+  fuzzy_browser = lib.getExe (pkgs.writeShellApplication {
+    name = "fuzzy_browser";
+    runtimeInputs = with pkgs; [ qutebrowser tofi ];
+    text = ''
+      quickmarks=$(cut -f1 -d\ < "$XDG_CONFIG_HOME/qutebrowser/quickmarks" )
+      query=$(printf "%s\n" "$quickmarks" | tofi  --text-cursor=true --prompt-text "󰜏 " --require-match=false )
+      [ -z "$query" ] || qutebrowser --target window ":open -t $query"
+    '';
+  }
+  );
+
 }
