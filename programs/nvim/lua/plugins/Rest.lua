a06220602d6c6eb97a6abb921e06da76592404cd
Lampros Pitsillos <hauahx@gmail.com>
2025-11-03 07:16:53 +0200

    added kulala.nvim

---
+++ programs/nvim/lua/plugins/Rest.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "mistweaverco/kulala.nvim",
+    keys = {
+      { "<leader>Rs", desc = "Send request" },
+      { "<leader>Ra", desc = "Send all requests" },
+      { "<leader>Rb", desc = "Open scratchpad" },
+    },
+    ft = {"http", "rest"},
+    opts = {
+      global_keymaps = false,
+      global_keymaps_prefix = "<leader>R",
+      kulala_keymaps_prefix = "",
+    },
+  },
+}
