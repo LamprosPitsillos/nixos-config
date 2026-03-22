e494c9364adfbfd25e140d899dda949bfbfdbf6b
Lampros Pitsillos <hauahx@gmail.com>
2026-03-22 19:48:18 +0200

    change ip for homelab

--- hosts/homelab/networking.nix
+++ hosts/homelab/networking.nix
@@ -9,25 +9,44 @@ in
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzX5efY+WMjdQrkbVphp4RRWQVh8vypslOylE06uHA6"
     ];
   };
-
   networking = {
+
     hostName = host; # Define your hostname.
-    networkmanager.enable = true; # Easiest to use and most distros use this by default.
+    networkmanager.enable = false;
     enableIPv6 = false;
+
+    useDHCP = false;
+
+    interfaces.eth0 = {
+      ipv4.addresses = [
+        {
+          address = "192.168.31.150";
+          prefixLength = 24;
+        }
+      ];
+    };
+
+    defaultGateway = "192.168.31.1";
+
+    nameservers = [
+      "1.1.1.1"
+      "8.8.8.8"
+    ];
+
+    nftables.enable = true;
+
+    firewall = {
+      enable = true;
+      logRefusedConnections = true;
+      # Always allow traffic from your Tailscale network
+      trustedInterfaces = [ "tailscale0" ];
+      # Allow the Tailscale UDP port through the firewall
+      allowedUDPPorts = [ config.services.tailscale.port ];
+    };
   };
 
   # 1. Enable the service and the firewall
   services.tailscale.enable = true;
-  networking.nftables.enable = true;
-
-  networking.firewall = {
-    enable = true;
-    logRefusedConnections = true;
-    # Always allow traffic from your Tailscale network
-    trustedInterfaces = [ "tailscale0" ];
-    # Allow the Tailscale UDP port through the firewall
-    allowedUDPPorts = [ config.services.tailscale.port ];
-  };
 
   # 2. Force tailscaled to use nftables (Critical for clean nftables-only systems)
   # This avoids the "iptables-compat" translation layer issues.
